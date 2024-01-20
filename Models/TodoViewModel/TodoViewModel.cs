b4610504c8861e6e9c96e76611d10861305ff68c
Elise-Brook <153657407+EliseBrook@users.noreply.github.com>
2024-01-20 11:04:39 +0000

    Add files via upload

---
+++ Models/TodoViewModel/TodoViewModel.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace Todo.Models.ViewModels
+{
+
+    public class TodoViewModel
+    {
+
+        public List<TodoItem> TodoList {get; set; }
+
+        public TodoItem Todo {get; set;}
+    }
+}
